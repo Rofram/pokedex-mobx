14af90acd36bee08625971a315d4ec22d1a988c5
Rodrigo de França <howdrigo.fr@gmail.com>
2022-04-22 03:47:19 -0300

    feat: implemented pagination and filter

--- src/Pokedex/PokemonList.tsx
+++ src/Pokedex/PokemonList.tsx
@@ -1,10 +1,26 @@
-import { useQuery } from "react-query";
+import { useInfiniteQuery } from "react-query";
+import { Link } from "react-router-dom";
+import { listFetcher } from "../services/Api";
+import PokemonListItem from "./PokemonListItem";
+import styles from './PokemonList.module.scss'
+import { observer } from "mobx-react-lite";
 
-const PokemonList = () => {
-  const {data, isLoading} = useQuery('pokemon-list', 
-    async () => await fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=0').then(res => res.json()), 
+type PokemonListProps = {
+  filter: (e: any) => boolean;
+}
+
+const PokemonList = ({ filter }: PokemonListProps) => {
+  const {
+    data, 
+    isLoading, 
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage
+  } = useInfiniteQuery('pokemon-list', 
+    listFetcher(), 
     {
-      staleTime: Infinity,
+      staleTime: 600_000,
+      getNextPageParam: (lastPage, pages) => lastPage.nextCursor,
   });
 
   return (
@@ -12,14 +28,31 @@ const PokemonList = () => {
       {isLoading ? (
         <div>Loading...</div>
       ) : (
-        <ul>
-          {data.results.map((pokemon: any) => (
-            <li key={pokemon.name}>{pokemon.name}</li>
-          ))}
-        </ul>
+        <>
+          {data?.pages.map((page) => page.filter(filter).map((pokemon: any) => (
+            <Link 
+              key={pokemon.name}
+              to={`/details/${pokemon.name}`}
+              className={styles['pokemon-link']}
+            >
+              <PokemonListItem {...pokemon} />
+            </Link>
+          )))}
+          {isFetchingNextPage
+            ? 'Loading more...'
+            : hasNextPage
+            ? 'Load More'
+            : 'Nothing more to load'}
+          <button 
+            onClick={() =>fetchNextPage()}
+            className={styles['load-more-btn']}
+          >
+            Load more
+          </button>
+        </>
       )}
     </>
   )
 }
 
-export default PokemonList;
+export default observer(PokemonList);
