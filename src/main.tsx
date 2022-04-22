a05cc8ebbd1cc6b7bb0869795c7cbcef839396dc
Rodrigo de França <howdrigo.fr@gmail.com>
2022-04-22 03:49:31 -0300

    feat: add store provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import store, { StoreContext } from './stores'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <StoreContext.Provider value={store}>
+      <App />
+    </StoreContext.Provider>
   </React.StrictMode>
 )
